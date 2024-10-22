fa31c77ff14474d743805be6f8ead15cbc98444c
Jolita Gedminaitė <jolita.gedminaite1@gmail.com>
2024-10-22 10:47:52 +0300

    color

--- src/components/home/EducationCard.jsx
+++ src/components/home/EducationCard.jsx
@@ -8,7 +8,7 @@ const EducationCard = ({ data }) => {
   return (
     <Col lg="6">
       <div className="pb-5 text-center">
-        <img className=" bg-white mb-3" src={data.logo} alt="" />
+        <img className=" bg-white m-0 mb-3" src={data.logo} alt="" />
         <p className="lead">
           {data.title}
           <br />
