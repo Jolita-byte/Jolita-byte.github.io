095d05a424b204873ec7bda4c68a6a2116027f3f
Jolita Gedminaitė <jolita.gedminaite1@gmail.com>
2024-11-07 10:09:20 +0200

    color

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,7 +6,7 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   // gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #000000",
+  gradientColors: "#4484ce, #1ad7c0, #1a49d7, #9b59b6, #ff7f7f, #000000",
   firstName: "Jolita",
   middleName: "",
   lastName: "Gedminaitė",
